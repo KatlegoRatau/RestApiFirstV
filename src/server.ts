2cee26990cb39dba01f26670eb983a5e50cb9230
Katlego <Katlego@academy.local>
2018-10-10 08:57:40 +0200

    first commit rest api serverless

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import * as express from "express";
+import {Server, Path, GET, PathParam} from "typescript-rest";
+ 
+@Path("/hello")
+class HelloService {
+  @Path(":name")
+  @GET
+  sayHello( @PathParam('name') name: string): string {
+    return "Hello " + name;
+  }
+}
+
+@Path("/users")
+class UserService {
+   @Path("/:userId/books/:bookId")
+   @GET
+   getUserBook(@PathParam("userId") userId: number, @PathParam("bookId") bookId: number) {
+     return userId+ " "+bookId;
+   }
+}
+ 
+let app: express.Application = express();
+Server.buildServices(app);
+ 
+app.listen(4200, function() {
+  console.log('Rest Server listening on port 4200!');
+});
